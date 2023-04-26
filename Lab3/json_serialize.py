fe46917ff683235d5fc5b3dfc86cf7d243cac6ec
Тимофеев Кирилл 153505 <otval1te@yandex.by>
2023-04-26 14:39:49 +0300

    it works. i don't like it. me

--- Lab3/json_serialize.py
+++ Lab3/json_serialize.py
@@ -1,23 +1,25 @@
 import inspect
-import functools
+import types
 import re
 
 
 def shield_str(txt: str) -> str:
-    txt = txt.replace('\\', '\\\\')
+    txt = txt.replace("\\", "\\\\")
     txt = txt.replace('"', '\\"')
     txt = txt.replace("'", "\\'")
-    txt = txt.replace('\n', '\\n')
-    txt = txt.replace('\t', '\\t')
+    txt = txt.replace("\n", "\\n")
+    txt = txt.replace("\t", "\\t")
 
     return txt
 
-def change_indent(src:str) -> str:
-    while(src[:3] != 'def'):
-        src = src.replace('    def', 'def', 1)
-        src = src.replace('\n    ', '\n')
-    
+
+def change_indent(src: str) -> str:
+    while src[:3] != "def":
+        src = src.replace("    def", "def", 1)
+        src = src.replace("\n    ", "\n")
+
     return src
+
 
 def f():
     pass
@@ -29,13 +31,25 @@ none = type(None)
 
 
 def serialize(obj):
-    default_types = [int, str, bool, dict, tuple,
-                     float, list, set, bool, type,
-                     func, module, type]
+    default_types = [
+        int,
+        str,
+        bool,
+        dict,
+        tuple,
+        float,
+        list,
+        set,
+        bool,
+        type,
+        func,
+        module,
+        type,
+    ]
     if type(obj) in default_types:
-        return '{' + basic_serailize(obj) + '}'
+        return "{" + basic_serailize(obj) + "}"
     else:
-        return '{' + serialize_none() + '}'
+        return "{" + serialize_none() + "}"
 
 
 basic: str = ' "type": "{type}", "type properties": {t_p}, "value": {val} '
@@ -68,125 +82,135 @@ def basic_serailize(obj) -> str:
     else:
         return serialize_none()
 
-def type_to_dict(obj:type) -> dict:
+
+def type_to_dict(obj: type) -> dict:
     dct = obj.__dict__
-    
-    ret_dct:dict = {'__name__': obj.__name__}
-    for k,v in dct.items():
-        if k == '__module__':
+
+    ret_dct: dict = {"__name__": obj.__name__}
+    for k, v in dct.items():
+        if k == "__module__":
             continue
-        
+
         ret_dct[k] = v
 
         # print(ret_dct)
     return ret_dct
-        
-def serialize_none():
-    return basic.format(type='none', t_p='{}', val='{}')
 
-def serialize_type(obj:type) -> str:
+
+def serialize_none():
+    return basic.format(type="none", t_p="{}", val="{}")
+
+
+def serialize_type(obj: type) -> str:
     # print(obj.__dict__)
     dct = type_to_dict(obj)
-    
-    return basic.format(type='type', t_p='{}', val = dict_jsonobj(dct))
-        
+
+    return basic.format(type="type", t_p="{}", val=dict_jsonobj(dct))
+
 
 def serialize_module(obj):
     val_dict = {}
     line = str(obj)
-    srch = re.search(r'\\\\', line)
+    srch = re.search(r"\\\\", line)
     if srch is None:
-        val_dict['path'] = 'basic'
+        val_dict["path"] = "basic"
     else:
-        val_dict['path'] = re.search(r'from \'(.*)\'', line).group(1)
+        val_dict["path"] = re.search(r"from \'(.*)\'", line).group(1)
 
-    val_dict['name'] = re.search(r'module \'([^\']*)\'', line).group(1)
+    val_dict["name"] = re.search(r"module \'([^\']*)\'", line).group(1)
 
-    return basic.format(type='module', t_p='{}', val=dict_jsonobj(val_dict))
+    return basic.format(type="module", t_p="{}", val=dict_jsonobj(val_dict))
 
 
 def serialize_tuple(obj):
     val = "["
     for it in obj:
-        val += serialize(it) + ', '
+        val += serialize(it) + ", "
 
-    val = val[:-2] + ']'
+    val = val[:-2] + "]"
 
-    return basic.format(type="tuple", val=val, t_p='{}')
+    return basic.format(type="tuple", val=val, t_p="{}")
 
 
 def serialize_set(obj):
     val = "["
     for it in obj:
-        val += serialize(it) + ', '
+        val += serialize(it) + ", "
 
-    val = val[:-2] + ']'
+    val = val[:-2] + "]"
 
-    return basic.format(type="set", val=val, t_p='{}')
+    return basic.format(type="set", val=val, t_p="{}")
 
 
 def serialize_bool(obj) -> str:
-    return basic.format(type="bool", val=str(obj).lower(), t_p='{}')
+    return basic.format(type="bool", val=str(obj).lower(), t_p="{}")
 
 
 def serialize_int(obj) -> str:
-    return basic.format(type="int", val=str(obj), t_p='{}')
+    return basic.format(type="int", val=str(obj), t_p="{}")
 
 
 def serialize_float(obj) -> str:
-    return basic.format(type="float", val=str(obj), t_p='{}')
+    return basic.format(type="float", val=str(obj), t_p="{}")
 
 
 def serialize_string(obj) -> str:
-    return basic.format(type="str", val='"'+shield_str(obj)+'"', t_p='{}')
+    return basic.format(type="str", val='"' + shield_str(obj) + '"', t_p="{}")
 
 
 def serialize_dict(obj: dict) -> str:
-    val = '['
+    val = "["
     for it in obj.items():
-        val += '{ "key": ' + \
-            serialize(it[0]) + ', "value": ' + serialize(it[1]) + '}, '
+        val += '{ "key": ' + serialize(it[0]) + ', "value": ' + serialize(it[1]) + "}, "
 
-    val = val[:-2] + ']'
-    if val == ']':
-        val = '[]'
-    return basic.format(type="dict", val=val, t_p='{}')
+    val = val[:-2] + "]"
+    if val == "]":
+        val = "[]"
+    return basic.format(type="dict", val=val, t_p="{}")
 
 
 def serialize_list(obj):
     val = "["
     for it in obj:
-        val += serialize(it) + ', '
+        val += serialize(it) + ", "
 
-    val = val[:-2] + ']'
-    if val == ']':
-        val = '[]'
-    return basic.format(type="list", val=val, t_p='{}')
+    val = val[:-2] + "]"
+    if val == "]":
+        val = "[]"
+    return basic.format(type="list", val=val, t_p="{}")
+
+
+def serialize_func2(obj: func):
+    types.FunctionType()
+    obj.__code__
+    val_dict = {}
+    glbs = {}
+    nonlocls = {}
 
 
 def serialize_func(obj):
     val_dict = {}
     lines = change_indent(inspect.getsource(obj))
-    name_regex = r'def\s+(\w+)\s*\('
+    name_regex = r"def\s+(\w+)\s*\("
     func_name = re.search(name_regex, lines)
-    val_dict['name'] = func_name.group(1)
-    val_dict['source lines'] = lines
+    val_dict["name"] = func_name.group(1)
+    val_dict["source lines"] = lines
 
     cl_vars = inspect.getclosurevars(obj)
-    val_dict['globals'] = dict(cl_vars.globals)
-    val_dict['nonlocals'] = dict(cl_vars.nonlocals)
+    val_dict["globals"] = dict(cl_vars.globals)
+    val_dict["nonlocals"] = dict(cl_vars.nonlocals)
 
     value = dict_jsonobj(val_dict)
 
-    return basic.format(type='function', val=value, t_p='{}')
+    return basic.format(type="function", val=value, t_p="{}")
 
 
 def dict_jsonobj(d: dict) -> str:
-    rstr = '{'
+    rstr = "{"
     for key, val in d.items():
         rstr += f'"{key}": {serialize(val)}, '
 
     rstr = rstr[:-2:]
-    rstr += '}'
+    rstr += "}"
 
     return rstr
--- Lab3/main.py
+++ Lab3/main.py
@@ -5,12 +5,25 @@ import math
 import sys
 import os.path
 import importlib
+import types
 
 path = "/home/user/Documents/SCI/Lab3/"
 
 
+def funcuc():
+    cl_var = None
+
+    def uhuhu():
+        nonlocal cl_var
+
+    return uhuhu
+
+
+Cell = type(funcuc().__closure__[0])
+
+
 class Sass:
-    def __init__(self, a:str, b:int):
+    def __init__(self, a: str, b: int):
         self.a = a
         self.b = b
         self.__hop = "hop" + a
@@ -30,73 +43,81 @@ class Sass:
             //"field":value
         }
         """
+
     def __add__(self, other):
         return 1
-    
+
     def __private(self):
-        print ('private')
-        
+        print("private")
+
     def _protected():
-        print('protected')
-    
+        print("protected")
+
     def prpuk(self):
         print(self.__puk)
-    
+
+
+def dec(func):
+    def d(*args):
+        print("it's decoratin' time")
+        return func(*args)
+
+    return d
+
 
 i = 5
 
 
-def f(a: int):
-    print(8)
-    h = 7 + 7.4
-    print(h)
-    print(i)
-    print(math.sin(i * a))
-    return "kikiki"
+# @dec
 
-def print_tuple_list(d:list):
-    for k,v in d:
+
+def print_tuple_list(d: list):
+    for k, v in d:
         print(str(k) + " : " + str(v))
-        
-def print_dict(d:dict):
-    for k,v in d.items():
+
+
+def print_dict(d: dict):
+    for k, v in d.items():
         print(str(k) + " : " + str(v))
-        
+
 
 def main():
-    # d = {"one": 1, "string": "str", "float": 1.25, "bool": False,
-    #      "set": {1, 2, 3}, "list": [], "tuple": (1, 2, 3), 'empty dict': {}}
-    # ser = serlib.serialize(f)
-    # print(ser)
-    # ff = serlib.deserialize(ser)
-    # print(ff(1))
-    t:type = Sass
-    s = Sass('ggg', 3)
-    # print(dir(s))
-    # print(dir(Sass))
-    # print(vars(s))
-    # print(vars(Sass))
-    # print_tuple_list(inspect.getmembers(t))
-    # print()
-    
-    # print_dict(t.__dict__)
-    sr = serlib.serialize(Sass)
-    print(sr)
-    print()
-    print()
-    t = serlib.deserialize(sr)
-    s = t('ggg', 3)
-    s.ppp(6)
-    print(t)
-    
-    
-    #print(type(s.__dict__))
-   
-    print()
-   # print_dict(s.__dict__)
-    print()
+    t = 0
 
-    
+    @dec
+    def f(a: int):
+        print(8)
+        h = 7 + 7.4
+        print(h)
+        print(i)
+        print(math.sin(i * a + t))
+        return "kikiki"
+
+    # code = f.__code__
+    # for smth in code.co_lines():
+    #     print(smth)
+
+    # print(code.co_linetable.__str__())
+    # print(code.co_lnotab)
+
+    # print_tuple_list(inspect.getmembers(code))
+    # print(f.__closure__)
+    # print(f.__globals__)
+    code = f.__code__
+    gl = dict(inspect.getclosurevars(f).globals)
+    gl.update(inspect.getclosurevars(f).nonlocals)
+    print(gl)
+    defs = inspect.getfullargspec(f).defaults
+    closure = inspect.getclosurevars(f).nonlocals
+
+    cells = []
+    for key, clvar in closure.items():
+        print("closure be like " + str(clvar))
+        kkk = Cell(clvar)
+        cells.append(kkk)
+
+    fff = types.FunctionType(code, dict(gl), "f", defs, tuple(cells))
+    print(fff(7))
 
 
 if __name__ == "__main__":
